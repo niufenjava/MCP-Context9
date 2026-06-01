83074931c86942a26d73f81aeb7ae02ce6090efd
haijun.zhang <haijun.zhang@cotticoffee.com>
2026-06-01 09:53:22 +0800

    feat: switch to smaller embedder model BAAI/bge-small
    - Smaller model: 45MB vs 90MB
    - Faster inference
    - Note: will need to re-download model and re-index

--- indexer/embedder.py
+++ indexer/embedder.py
@@ -1,7 +1,7 @@
 from fastembed import TextEmbedding
 
 class Embedder:
-    def __init__(self, model_name: str = "BAAI/bge-small-en-v1.5"):
+    def __init__(self, model_name: str = "BAAI/bge-small"):
         self.model_name = model_name
         self.model = TextEmbedding(model_name=model_name)
 
